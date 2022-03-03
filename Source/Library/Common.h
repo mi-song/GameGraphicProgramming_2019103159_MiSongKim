d53e6691598e7ecbb37c820630ddb19bee91fe4e
mi-song <kmsong0@naver.com>
2022-03-03 19:10:15 +0900

    Add project files.

---
+++ Source/Library/Common.h
@@ -0,0 +1,15 @@
+﻿/*+===================================================================
+  File:      COMMON.H
+  Summary:   Common header file that contains common header files and
+			 macros used for the Library project of Game Graphics
+			 Programming course.
+  Functions:
+  © 2022 Kyung Hee University
+===================================================================+*/
+#pragma once
+
+#ifndef  UNICODE
+#define UNICODE
+#endif // ! UNICODE
+
+#include <windows.h>
